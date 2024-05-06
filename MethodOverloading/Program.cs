3b9576df834cb9976d5c7adb4c4a6deebd75d74d
Brooks Stockley <bstockley26@gmail.com>
2024-05-06 11:41:23 -0400

    finised

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Add(2, 4);
+
+            Add(3.8m, 4.9m);
+
+            
+            decimal one=.50m;
+            decimal two=.50m;
+
+            var addition = one + two;
+
+            var dollars = addition == 1;
+
+            Console.WriteLine(dollars);
+        }
+        static void Add(int one, int two)
+        {
+            var addition = (one + two);
+
+            Console.WriteLine(addition);
+        }
+        static void Add(decimal one, decimal two)
+        {
+            var addition = one + two;
+
+            Console.WriteLine(addition);
+        }
+        //static string Add(int one, int two, bool dollars)
+        //{
+
+        //    var addition = one + two;
+
+        //    dollars = addition == 1;
+
+        //    Console.WriteLine(dollars);
+
+        //    if (dollars = true)
+        //    {
+
+
+        //        Console.WriteLine($"{addition} dollars");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"{addition} dollar");
+        //    }
+
         }
     }
-}
